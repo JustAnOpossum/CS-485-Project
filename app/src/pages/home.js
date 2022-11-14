bfd08dd9dc09c03acc5deb7fc7bf8a5ef5ef7584
River-rs <rvrstein@gmail.com>
2022-11-13 17:33:30 -0700

    installations complete

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,8 +1,10 @@
 import './styles.css';
+import '../images/banner.jpg';
 
 const Home = () => {
   return (
     <div className='container'>
+      <div><img src='../images/banner.jpg'></img></div>
       <h3 className='heading'>Home</h3>
     </div>
   );
